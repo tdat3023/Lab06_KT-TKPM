43b4d84aeb612a42d03606704231f3b2ca31459e
tdat3023 <tdat3023@gmail.com.com>
2024-03-15 16:12:09 +0700

    add gateway

---
+++ gatewaydemo/src/main/java/gatewaydemo/com/GatewaydemoApplication.java
@@ -0,0 +1,13 @@
+package gatewaydemo.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewaydemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewaydemoApplication.class, args);
+	}
+
+}
---
+++ gatewaydemo/src/test/java/gatewaydemo/com/GatewaydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package gatewaydemo.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewaydemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
